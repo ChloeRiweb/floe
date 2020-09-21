c9e136e34cb6c1fe4e0eeb7db87ae370a713780b
chloerihouay <chloerihouay@hotmail.com>
2020-09-21 10:55:11 +0200

    db done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_21_081937) do
+ActiveRecord::Schema.define(version: 2020_09_21_085405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 2020_09_21_081937) do
     t.string "embroidery"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "purchases", force: :cascade do |t|
@@ -33,7 +35,9 @@ ActiveRecord::Schema.define(version: 2020_09_21_081937) do
     t.bigint "product_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
     t.index ["product_id"], name: "index_purchases_on_product_id"
+    t.index ["user_id"], name: "index_purchases_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -48,5 +52,7 @@ ActiveRecord::Schema.define(version: 2020_09_21_081937) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "users"
   add_foreign_key "purchases", "products"
+  add_foreign_key "purchases", "users"
 end
