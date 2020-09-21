0a2362ae002fbbf8e586445b2d364403fcc2b6de
chloerihouay <chloerihouay@hotmail.com>
2020-09-21 12:47:05 +0200

    index products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
   has_many :purchases
   belongs_to :user
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :color, presence: true
+  validates :size, presence: true
+  validates :gender, presence: true
 end
